6a9f1d37a99e47f0a995b8c50223accdc1fc24d5
Aaron Kutch <aaronkutch@att.net>
2023-07-23 18:22:17 -0500

    begin testing with cli

--- tests/src/bin/onex_standalone.rs
+++ tests/src/bin/onex_standalone.rs
@@ -2,20 +2,23 @@ use std::time::Duration;
 
 use common::container_runner;
 use onomy_test_lib::{
-    cosmovisor::{cosmovisor_get_addr, cosmovisor_start, sh_cosmovisor_no_dbg},
+    cosmovisor::{
+        cosmovisor_get_addr, cosmovisor_start, sh_cosmovisor, sh_cosmovisor_no_dbg,
+        sh_cosmovisor_tx,
+    },
     dockerfiles::onomy_std_cosmos_daemon,
     onomy_std_init,
     setups::market_standalone_setup,
     super_orchestrator::{
         sh,
-        stacked_errors::{MapAddError, Result},
+        stacked_errors::{Error, MapAddError, Result},
         Command, FileOptions,
     },
     Args, TIMEOUT,
 };
 use tokio::time::sleep;
 
-const CHAIN_ID: &str = "appname";
+const CHAIN_ID: &str = "onex";
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -27,17 +30,13 @@ async fn main() -> Result<()> {
             _ => format!("entry_name \"{s}\" is not recognized").map_add_err(|| ()),
         }
     } else {
-        let mut cmd = Command::new("go build ./cmd/standalone", &[]).ci_mode(true);
-        cmd.cwd = Some("./../multiverse/".to_owned());
+        let mut cmd = Command::new("go build ./cmd/onexd", &[]).ci_mode(true);
+        cmd.cwd = Some("./../market/".to_owned());
         let comres = cmd.run_to_completion().await?;
         comres.assert_success()?;
-        sh("cd ./../manual_test/", &[]).await?;
         // copy to dockerfile resources (docker cannot use files from outside cwd)
         sh(
-            &format!(
-                "cp ./../multiverse/standalone \
-                 ./tests/dockerfiles/dockerfile_resources/{CHAIN_ID}d"
-            ),
+            &format!("cp ./../market/onexd ./tests/dockerfiles/dockerfile_resources/{CHAIN_ID}d"),
             &[],
         )
         .await?;
@@ -54,21 +53,126 @@ async fn main() -> Result<()> {
     }
 }
 
+pub struct CoinPair {
+    coin_a: String,
+    coin_b: String,
+}
+
+impl CoinPair {
+    pub fn new(coin_a: &str, coin_b: &str) -> Result<Self> {
+        if coin_a >= coin_b {
+            Err(Error::from("coin_a >= coin_b, should be coin_a < coin_b"))
+        } else {
+            Ok(CoinPair {
+                coin_a: coin_a.to_owned(),
+                coin_b: coin_b.to_owned(),
+            })
+        }
+    }
+
+    pub fn coin_a(&self) -> &str {
+        &self.coin_a
+    }
+
+    pub fn coin_b(&self) -> &str {
+        &self.coin_b
+    }
+
+    pub fn coin_a_amount(&self, amount: u128) -> String {
+        format!("{}{}", amount, self.coin_a())
+    }
+
+    pub fn coin_b_amount(&self, amount: u128) -> String {
+        format!("{}{}", amount, self.coin_b())
+    }
+
+    pub fn paired(&self) -> String {
+        format!("{},{}", self.coin_a(), self.coin_b())
+    }
+}
+
+// cosmovisor run tx market create-pool 10000000anative 10000000afootoken --from
+// validator --fees 1000000anative -y -b block
+
+/// Initiates the pool with 1 of each coin
+pub async fn market_create_pool(coin_pair: &CoinPair) -> Result<()> {
+    sh_cosmovisor_tx("market create-pool", &[
+        &coin_pair.coin_a_amount(1),
+        &coin_pair.coin_b_amount(1),
+        "--from",
+        "validator",
+        "--fees",
+        "1000000anative",
+        "-y",
+        "-b",
+        "block",
+    ])
+    .await?;
+    Ok(())
+}
+
+//pool:
+//  denom1: afootoken
+//  denom2: anative
+//  drops: "2"
+//  leaders:
+//  - address: onomy1nvsmtc4trpwxrx4vyzlm4ex6e4q3y46wwyapr9 drops: "2"
+//  pair: afootoken,anative
+pub async fn market_show_pool(coin_pair: &CoinPair) -> Result<String> {
+    sh_cosmovisor("query market show-pool", &[&coin_pair.paired()]).await
+}
+
+// shows both sides, with one looking like
+//member:
+//  balance: "1"
+//  denomA: anative
+//  denomB: afootoken
+//  limit: "0"
+//  pair: afootoken,anative
+//  previous: "0"
+//  stop: "0"
+pub async fn market_show_members(coin_pair: &CoinPair) -> Result<(String, String)> {
+    let member_a = sh_cosmovisor("query market show-member", &[
+        &coin_pair.coin_a(),
+        &coin_pair.coin_b(),
+    ])
+    .await?;
+    let member_b = sh_cosmovisor("query market show-member", &[
+        &coin_pair.coin_b(),
+        &coin_pair.coin_a(),
+    ])
+    .await?;
+    Ok((member_a, member_b))
+}
+
+/*
+cosmovisor run query market list-drop
+drop:
+- active: true
+  drops: "2"
+  owner: onomy1r5q7yrqexn7dyy9uvf3p28raw7mxhc23jwura8
+  pair: afootoken,anative
+  sum: "2"
+  uid: "1"
+pagination:
+  next_key: null
+  total: "0"
+*/
+
+//cosmovisor run tx market redeem-drop 1 --from validator --fees 1000000anative
+// -y -b block
+
 async fn standalone_runner(args: &Args) -> Result<()> {
     let daemon_home = args.daemon_home.as_ref().map_add_err(|| ())?;
     market_standalone_setup(daemon_home, CHAIN_ID).await?;
     let mut cosmovisor_runner = cosmovisor_start(&format!("{CHAIN_ID}d_runner.log"), None).await?;
 
-    let addr = &cosmovisor_get_addr("validator").await?;
-    sh(
-        &format!(
-            "cosmovisor run tx bank send {addr} onomy1a69w3hfjqere4crkgyee79x2mxq0w2pfj9tu2m \
-             1337anative --gas auto --gas-adjustment 1.3 -y -b block"
-        ),
-        &[],
-    )
-    .await?;
+    let _addr = &cosmovisor_get_addr("validator").await?;
 
+    let coin_pair = CoinPair::new("afootoken", "anative").map_add_err(|| ())?;
+    market_create_pool(&coin_pair).await.map_add_err(|| ())?;
+
+    sleep(TIMEOUT).await;
     sleep(Duration::ZERO).await;
     cosmovisor_runner.terminate(TIMEOUT).await?;
     // test that exporting works
